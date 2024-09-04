62433ccef90a5698126bde23273cb25e8e70a3f0
ItalloK <itallo016@gmail.com>
2024-09-04 11:44:47 -0300

    att

---
+++ Estrutura de Dados/Algumas atividades online/Atvd11.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+/* Questão: Crie um programa que contenha um array contendo 5 elementos inteiros. Leia esse array do teclado e imprima 
+o enderec¸o das posicoes contendo valores pares. */
+
+int main(){
+
+    
+
+    return 0;
+}
