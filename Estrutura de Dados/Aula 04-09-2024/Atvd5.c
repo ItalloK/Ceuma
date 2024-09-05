fb97022228bc29338379863821b98de0e20bafab
ItalloK <itallo016@gmail.com>
2024-09-04 22:32:33 -0300

    att

--- Estrutura de Dados/Aula 04-09-2024/Atvd5.c
+++ Estrutura de Dados/Aula 04-09-2024/Atvd5.c
@@ -5,8 +5,22 @@ mesmos.*/
 quantos elementos são pares e quantos são ímpares, bem como a soma dos números pares e a soma dos números ímpares. */
 
 int main(){
-
-
-
+    int mat[3][4], sumPar = 0, sumImpar = 0, *p, contPar = 0, contImpar = 0;
+    for(int i = 0; i<3; i++){
+        for(int j = 0; j<4; j++){
+            p = &mat[i][j];
+            printf("Digite um valor para a posicao [%d][%d]: \n", i, j);
+            scanf("%d", p);
+            if(*p % 2 == 0){
+                sumPar+= *p;
+                contPar++;
+            }else{
+                sumImpar += *p;
+                contImpar++;
+            }
+        }
+    }
+    printf("Quantidade de pares: [%d], somatorio dos pares: [%d]\n",contPar, sumPar);
+    printf("Quantidade de impares: [%d], somatorio dos impares: [%d]\n",contImpar, sumImpar);
     return 0;
 }
