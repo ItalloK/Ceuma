c56dbce98a9e379bd097e2cc72ec35f6f9415069
ItalloK <itallo016@gmail.com>
2024-09-22 15:44:41 -0300

    att

--- Estrutura de Dados/Aula 18-09-2024/Atvd2.c
+++ Estrutura de Dados/Aula 18-09-2024/Atvd2.c
@@ -11,5 +11,50 @@
 
 int main(){
 
+    int tamanho, *vetor, opc;
+
+    printf("Digite o tamanho do vetor que deseja criar: \n");
+    scanf("%d", &tamanho);
+
+    vetor = malloc(tamanho * sizeof(int));
+    if(vetor == NULL){
+        return printf("Não há espaco para criar o vetor");
+    }
+
+    for(int i = 0; i<tamanho; i++){
+        printf("Digite o %dº valor: \n", i+1);
+        scanf("%d", &vetor[i]);
+    }
+
+    for(int i = (tamanho-1); i>=0; i--){
+        printf("%d \n", vetor[i]);
+    }
+
+    printf("Deseja alterar o tamanho dos vetores? [1 - para sim | 0 para não] \n");
+    scanf("%d", &opc);
+
+    if(opc == 0) return printf("Fim do Programa");
+    if(opc == 1){
+        vetor = realloc(vetor, tamanho * sizeof(int));
+    }
+    if(vetor == NULL){
+        return printf("Não há espaco para criar o vetor");
+    }
+    printf("Digite o novo tamanho do vetor que deseja criar: \n");
+    scanf("%d", &tamanho);
+
+    for(int i = 0; i<tamanho; i++){
+        printf("Digite o %dº valor: \n", i+1);
+        scanf("%d", &vetor[i]);
+    }
+
+    for(int i = (tamanho-1); i>=0; i--){
+        printf("%d \n", vetor[i]);
+    }
+
+    free(vetor);
+    vetor = NULL;
+    printf("Fim do programa.");
+    
     return 0;
 }
