5378f31209a3886841f58b61a73d000105fe329a
ItalloK <itallo016@gmail.com>
2024-09-22 19:38:41 -0300

    att

--- Estrutura de Dados/Aula 18-09-2024/Atvd3.c
+++ Estrutura de Dados/Aula 18-09-2024/Atvd3.c
@@ -8,5 +8,29 @@ Considere a média como sendo 7.0 */
 
 int main(){
 
+    int tam, *vetor;
+    float media = 0;
+
+    printf("Digite a quantidade de numeros que deseja:\n");
+    scanf("%d", &tam);
+
+    vetor = malloc(tam * sizeof(int));
+    if(vetor == NULL){
+        printf("Sem espaco.\n");
+        exit(1);
+    }
+
+    for(int i = 0; i<tam; i++){
+        printf("Digite o %dº numero: \n", i+1);
+        scanf("%d", &vetor[i]);
+        media += ((float)vetor[i])/tam;
+    }
+    printf("Media: %.2f, valores acima da media:\n", media);
+    for(int i = 0; i<tam; i++){
+        if(vetor[i] > media){
+            printf("%d\n", vetor[i]);
+        }
+    }
+
     return 0;
 }
