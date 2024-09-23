f4a4fcb4accd944d7cb60dd4fd5637e431e3e3f4
ItalloK <itallo016@gmail.com>
2024-09-22 22:43:54 -0300

    att

--- Estrutura de Dados/Aula 18-09-2024/Atvd4.c
+++ Estrutura de Dados/Aula 18-09-2024/Atvd4.c
@@ -9,5 +9,70 @@ os novos valores devem ser informados para completar. Imprima todos os valores p
 
 int main(){
 
+    int tamanho, oldTamanho, opc;
+    float *vetor;
+
+    printf("Qual tamanho do vetor que deseja? \n");
+    scanf("%d", &tamanho);
+
+    vetor = malloc(tamanho * sizeof(float));
+    if(vetor == NULL){
+        printf("Erro ao alocar espaco\n");
+        exit(1);
+    }
+
+    for(int i = 0; i<tamanho; i++){
+        printf("Digite o %dº valor:\n", i+1);
+        scanf("%f", &vetor[i]);
+    }
+    printf("valores positivos: \n");
+    for(int i = 0; i<tamanho; i++){
+        if(vetor[i] >= 0){
+            printf("%f\n", vetor[i]);
+        }
+    }
+
+    printf("Deseja aumentar o tamanho do vetor? [1 para sim | 0 para nao]\n");
+    scanf("%d", &opc);
+
+    if(opc == 0){
+        printf("fim do programa.\n");
+        exit(0);
+    }
+
+    if(opc == 1){
+        oldTamanho = tamanho;
+        printf("Qual tamanho do vetor que deseja? \n");
+        scanf("%d", &tamanho);
+        vetor = realloc(vetor, tamanho*sizeof(float));
+        if(vetor == NULL){
+            printf("Erro ao alocar espaco\n");
+            exit(1);
+        }
+    }
+
+    if(tamanho < oldTamanho){
+        printf("Voce escolheu um valor menor... valores positivos:\n");
+        for(int i = 0; i<tamanho; i++){
+            if(vetor[i] >= 0){
+                printf("%f\n", vetor[i]);
+            }
+        }
+    }
+
+    if(tamanho > oldTamanho){
+        for(int i = oldTamanho; i<tamanho; i++){
+            printf("Digite o %dº valor:\n", i+1);
+            scanf("%f", &vetor[i]);
+        }
+    }   
+
+    printf("valores positivos: \n");
+    for(int i = 0; i<tamanho; i++){
+        if(vetor[i] >= 0){
+            printf("%f\n", vetor[i]);
+        }
+    }
+
     return 0;
 }
