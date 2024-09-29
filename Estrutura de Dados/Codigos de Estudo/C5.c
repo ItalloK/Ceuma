8e029896a17fa3e095d802de7760bd8ff5b84d7e
ItalloK <itallo016@gmail.com>
2024-09-29 18:46:06 -0300

    att

--- Estrutura de Dados/Codigos de Estudo/C5.c
+++ Estrutura de Dados/Codigos de Estudo/C5.c
@@ -5,6 +5,27 @@
 uma função que vai ler os elementos desse vetor. Depois, no programa principal, o vetor preenchido deve ser impresso. 
 Além disso, antes de finalizar o programa, deve-se liberar a área de memória alocada. */
 
-void main(){
+void LerElementos(int tam, int *vet){
+    for(int i = 0; i<tam; i++){
+        printf("Digite o %dº elemento: \n", i+1);
+        scanf("%d", &vet[i]);
+    }
+}
 
+void main(){
+    int qntElementos, *vetor;
+    printf("Digite o tamanho do vetor que deseja criar: \n");
+    scanf("%d", &qntElementos);
+    vetor = malloc(qntElementos * sizeof(int));
+    if(vetor == NULL){
+        printf("Erro ao alocar memoria\n");
+        exit(1);
+    }
+    LerElementos(qntElementos, vetor);
+    printf("Valores lidos:\n");
+    for(int i = 0; i<qntElementos; i++){
+        printf("%dº val: %d\n", i+1, vetor[i]);
+    }
+    free(vetor);
+    vetor = NULL;
 }
