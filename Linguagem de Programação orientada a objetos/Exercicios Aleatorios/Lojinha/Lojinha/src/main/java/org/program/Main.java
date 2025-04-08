c0b818959bc8a153217103c03ded3c36637d94b4
Italo Gabriel <itallo016@gmail.com>
2025-04-08 19:12:03 -0300

    att

--- Linguagem de Programação orientada a objetos/Exercicios Aleatorios/Lojinha/Lojinha/src/main/java/org/program/Main.java
+++ Linguagem de Programação orientada a objetos/Exercicios Aleatorios/Lojinha/Lojinha/src/main/java/org/program/Main.java
@@ -11,6 +11,7 @@ public class Main {
         LojaRepository lojaRepository = new LojaRepository();
 
         Cliente cliente = new Cliente("Italo", "12345678912");
+        Cliente cliente2 = new Cliente("Usuario Teste 2", "98765432111");
         Arroz arroz = new Arroz(10, "111", 29, 5.00);
         Feijao feijao = new Feijao(10, "222", 9, 1.0);
 
@@ -19,6 +20,9 @@ public class Main {
         lojaRepository.listarProdutos();
         lojaRepository.listarClientes();
         lojaRepository.cadastrarCliente(cliente);
+        lojaRepository.cadastrarCliente(cliente2);
+        lojaRepository.removerCliente(cliente2);
+        lojaRepository.removerProduto(feijao);
 
 
         lojaRepository.realizarVenda(cliente, arroz, feijao);
